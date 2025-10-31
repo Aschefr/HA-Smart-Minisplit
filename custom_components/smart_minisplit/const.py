86d7a10aa22744ef90dff3f23435635b58a53bf6
Quentin <s.asche@yahoo.fr>
2025-10-31 11:52:28 +0100

    Update and rename const.py to const.py

---
+++ custom_components/smart_minisplit/const.py
@@ -0,0 +1,42 @@
+"""Constantes pour l'intégration HA Smart Minisplit"""
+
+DOMAIN = "smart_minisplit"
+PLATFORMS = ["climate", "sensor", "switch"]
+DEFAULT_NAME = "Smart Minisplit"
+DEFAULT_HYSTERESIS = 2.0
+DEFAULT_OFFSET = 1.0
+DEFAULT_CONSIGNES = {
+    "absence": {"chauffage": 18.0, "climatisation": 26.0},
+    "eco": {"chauffage": 20.0, "climatisation": 25.0},
+    "confort": {"chauffage": 22.0, "climatisation": 24.0}
+}
+
+# Modes
+MODE_ABSENCE = "absence"
+MODE_ECO = "eco"
+MODE_CONFORT = "confort"
+
+# Saisons
+SEASON_CHAUFFAGE = "chauffage"
+SEASON_CLIMATISATION = "climatisation"
+
+# États
+STATE_OFF = "off"
+STATE_ON = "on"
+STATE_AUTO = "auto"
+STATE_MANUAL = "manual"
+
+# Attributs de configuration
+CONF_MINI_SPLIT = "mini_split_climate"
+CONF_TEMP_EXT = "temperature_exterieure"
+CONF_TEMP_PIECE = "temperature_piece"
+CONF_PRESENCE_PIECE = "presence_piece"
+CONF_PRESENCE_MAISON = "presence_maison"
+CONF_HYSTERESIS = "hysteresis"
+CONF_OFFSET = "offset"
+CONF_CONSIGNE_ABSENCE_CHAUFFAGE = "consigne_absence_chauffage"
+CONF_CONSIGNE_ECO_CHAUFFAGE = "consigne_eco_chauffage"
+CONF_CONSIGNE_CONFORT_CHAUFFAGE = "consigne_confort_chauffage"
+CONF_CONSIGNE_ABSENCE_CLIMATISATION = "consigne_absence_climatisation"
+CONF_CONSIGNE_ECO_CLIMATISATION = "consigne_eco_climatisation"
+CONF_CONSIGNE_CONFORT_CLIMATISATION = "consigne_confort_climatisation"
